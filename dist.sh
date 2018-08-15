458af8b6748a9d12822b3faa21d4c61fc4f97bad
Phil Howard <phil@gadgetoid.com>
2018-08-15 12:35:26 +0000

    Added install endpoint for BG

--- dist.sh
+++ dist.sh
@@ -9,7 +9,11 @@ PYTHON_VERSIONS=(
 for ((i = 0; i < ${#PYTHON_VERSIONS[@]}; i++)); do
 	PYTHON_VERSION="${PYTHON_VERSIONS[$i]}"
 	PYTHON_PATH="/usr/bin/python$PYTHON_VERSION"
-	printf "Building for Python $PYTHON_VERSION\n"
-	$PYTHON_PATH setup.py bdist_wheel
+	if [[ -f "$PYTHON_PATH" ]]; then
+		printf "Building for Python $PYTHON_VERSION\n"
+		$PYTHON_PATH setup.py bdist_wheel
+	else
+		printf "$PYTHON_PATH not found for Python $PYTHON_VERSION\n"
+	fi
 done
 
---
+++ install.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+PYTHON_VERSIONS=(
+	"3.5"
+	"3.4"
+	"2.7"
+)
+
+printf "VL53L1X Python Library: Installer\n\n"
+
+if [ $(id -u) -ne 0 ]; then
+	printf "Script must be run as root. Try 'sudo ./install.sh'\n"
+	exit 1
+fi
+
+for ((i = 0; i < ${#PYTHON_VERSIONS[@]}; i++)); do
+	PYTHON_VERSION="${PYTHON_VERSIONS[$i]}"
+	PYTHON_PATH="/usr/bin/python$PYTHON_VERSION"
+	if [[ -f "$PYTHON_PATH" ]]; then
+		printf "Installing for Python $PYTHON_VERSION\n"
+		$PYTHON_PATH -m pip install vl53l1x
+	else
+		printf "Skipping Python $PYTHON_VERSION\n"
+	fi
+done
+
