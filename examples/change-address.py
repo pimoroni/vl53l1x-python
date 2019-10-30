382f6b2f94872f1249856281cd5f7136c004a483
Phil Howard <phil@gadgetoid.com>
2019-10-30 16:59:59 +0000

    Remove sensor output from change-address.py

--- examples/change-address.py
+++ examples/change-address.py
@@ -1,9 +1,5 @@
 #!/usr/bin/env python
 
-import os
-import time
-import sys
-import signal
 import argparse
 
 import VL53L1X
@@ -12,8 +8,10 @@ import VL53L1X
 addr_current = 0x29
 addr_desired = 0x33
 
+
 def auto_int(x):
     return int(x, 0)
+
 
 parser = argparse.ArgumentParser(description='Change address options.')
 parser.add_argument('--current', type=auto_int, help='The current VL53L1X i2c address.', default=addr_current)
@@ -35,30 +33,7 @@ Press Ctrl+C to exit.
 """.format(addr_current, addr_desired))
 
 
-"""
-Open and start the VL53L1X ranging sensor
-"""
 tof = VL53L1X.VL53L1X(i2c_bus=1, i2c_address=addr_current)
-tof.open() # Initialise the i2c bus and configure the sensor
+tof.open()
 tof.change_address(addr_desired)
 tof.close()
-tof.open()
-tof.start_ranging(1) # Start ranging, 1 = Short Range, 2 = Medium Range, 3 = Long Range
-
-
-running = True
-
-def exit_handler(signal, frame):
-    global running
-    running = False
-    tof.stop_ranging() # Stop ranging
-    print()
-    sys.exit(0)
-
-signal.signal(signal.SIGINT, exit_handler)
-
-while running:
-    distance_in_mm = tof.get_distance() # Grab the range in mm
-    print("Distance: {}mm".format(distance_in_mm))
-    time.sleep(0.1)
-
