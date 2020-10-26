ebe45b700c6ab38d226dede79c80a8f7729b8800
Naoki OGAWA <na-ogawa@aruka.sakura.ne.jp>
2020-10-26 15:49:32 +0900

    fixed a resource leak

--- python/VL53L1X.py
+++ python/VL53L1X.py
@@ -89,10 +89,12 @@ class VL53L1X:
         self._tca9548a_num = tca9548a_num
         self._tca9548a_addr = tca9548a_addr
 
-        self._i2c = SMBus(i2c_bus)
+        self._i2c = SMBus()
         try:
             if tca9548a_num == 255:
+                self._i2c.open(bus=self._i2c_bus)
                 self._i2c.read_byte_data(self.i2c_address, 0x00)
+                self._i2c.close()
         except IOError:
             raise RuntimeError("VL53L1X not found on adddress: {:02x}".format(self.i2c_address))
 
